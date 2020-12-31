cc07fe157b63745e54fbdd0325cb1e440def1c85
littleprincey <orlandow1928@gmail.com>
2021-01-01 12:18:01 +1300

    Add bookRoutes

--- server/db/booksDBFuncs.js
+++ server/db/booksDBFuncs.js
@@ -16,33 +16,16 @@ function getBook(id, db = connection){
 }
 // kind of like a find, run thru all of the books and find the plant whose id matches the id that has been passed in 
 
-// READ - Get User Books
-// function getUsersBook(id, db = connection) {
-//   return db('users_plants').select().where('user_id', id)
-// }
-
 
 //CREATE (Single Resource)
-function createBook(newPlant, db = connection){
+function addNewBookToDB(newBook, db = connection){
   return db('books').insert(newBook)
   // here we're going into the books db, inserting & automatically returning an array of ids for whatever thing(s) has been inserted, but we just want the id of the task we've added, hence the .then
   .then(ids => ids[0])
 }
 
-//
-// function addBookToProfileDbFunc(plant, db = connection) {
-//   return db('users_plants').insert(plant, 'id')
-// }
-
-// function deleteBook(id, db = connection) {
-//   return db('users_plants').delete().where('plant_id', id)
-// }
-
 module.exports = {
   getBooks,
   getBook,
-  // getUsersBook,
-  createBook,
-  // addBookToProfileDbFunc,
-  // deleteBook
+  addNewBookToDB
 }
---
+++ server/routes/bookRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const router = express.Router()
+const { getTokenDecoder } = require('authenticare/server')
+
+const { getBooks, getBook, addNewBookToDB } = require('../db/booksDBFuncs')
+
+// READ -- GET LIST OF BOOKS
+router.get('/', (req, res) => {
+    getBooks()
+        .then(books => {
+            res.json(books)
+            //res.json means stringify the response & put it in JSON format so we can then deal with it this way
+        })
+        .catch(err => {
+            console.log('catch', err)
+            res.status(500).json({ message: 'Something broke' })
+        })
+})
+
+// READ -- GET SINGLE BOOK 
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    getBook(id)
+        .then(book => {
+            res.json(book)
+            //res.json means res.send(JSON.stringify(plant))
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Something broke' })
+        })
+})
+
+// CREATE i.e ADD ONE NEW BOOK 
+router.post('/', (req, res) => {
+    const newBook = req.body
+    addNewBookToDB(newBook)
+        .then(id => {
+            res.json({ id: id })
+            // at this stage one bit of info we don't have is the auto generated id of the new plant, which is why we're passing this back as the response 
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Something broke' })
+        })
+})
+
+module.exports = router
