1174b26cc96291b55f9a1acbdc62673e7531e549
littleprincey <orlandow1928@gmail.com>
2021-01-01 15:30:25 +1300

    change webpack file

--- client/components/Books.jsx
+++ client/components/Books.jsx
@@ -3,13 +3,14 @@ import { connect } from 'react-redux'
 
 
 const Books = (props) => {
-  const books = props.books
+  
 
 
     return (
       <>
+        <h1>here is a list of incredible books!</h1>
         <div>
-          <h3>{books[0].title}</h3>
+          {/* <h3>{props.books[0].title}</h3> */}
         </div>
       </>
     )
@@ -17,7 +18,8 @@ const Books = (props) => {
 
 function mapStateToProps (globalState) {
   return {
-    books: globalState.books
+    books: globalState.books,
+    setLoaded: globalState.setLoaded
   }
 }
 
--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import reducers from './reducers'
 import App from './components/App'
