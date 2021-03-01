de5e09041d811657cfa99545645378d20c6fdab8
littleprincey <orlandow1928@gmail.com>
2021-03-01 16:21:36 +1300

    add about page w text

---
+++ client/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="">
+      <div>
+        <h1>about (c)literature!</h1> 
+        <p>
+          dear to the heart, this project has been in the works for a long time. it is a place where you can find the different works of artists and authors and creators in one place. These content creators are all voices that struggle to be heard. they are pushed to the margins and their vital perspectives are not given voice. 
+        </p>
+        <p>
+          after struggling for years to research things like sci-fi by indigenous authors - to see what kind of future a first nations person might envisage, or queer women perspectives on economics - it became obvious that centralising these amazing creations and resources would make it so much easier for them to be accessed. and they should be accessed. the wealth of beauty and wisdom to be found here is incredible. everyone should be able to hear these voices easily.
+        </p>
+      </div>
+      <div>
+        <h1>about orlando</h1> 
+        <p>
+          orlando suffered a severe burnout at 25 from general over-doing it behaviours. during the two years of their burnout, they learned. they learnt in new ways about new things in the hibernated state of burnout. they couldnt operate any longer as they had before, couldnt be in the world as they had before, so had to learn to do things differently. orlando discovered the power of art and poetry and fiction. they discovered radical slowness, care and warmth. during this transformation, research played a key role. being severely disabled forced them through different channels, down different wormholes. they discovered the atomic, lyrical poetry of Nayirrah Waheed, the radical, soft sermons of The Nap Ministry, the imagination and wonder of Becky Chambers. 
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Nav
